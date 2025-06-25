8884dc9717905156acaa46a8973bc7ce2b325be9
Retro-Jbit-Anon <jidaarabbas@gmail.com>
2025-06-26 01:07:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+import plotly as px
 
 # Generate synthetic data
 def generate_nclh_data(num_entries=150):
